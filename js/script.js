bb3769ef3915c6f7a8b81614827fba755dc2e3c9
mjrdnk <mjrdnk@gmail.com>
2016-04-15 21:23:19 +0200

    jquery sliding trying

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
 	
+	//preset screens
+	
+	
+	// behaviour on click
+	$('#performer').click(function() {
+		$(this).toggle('slide', {direction: 'right'}, 1000);
+	});
+
+	// behaviour on click
+	$('#lurker').click(function() {
+		$(this).toggle('slide', {direction: 'left'}, 1000);
+	});
+
 });
