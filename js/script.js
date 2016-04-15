0f0928f2f985847b1590395a4c3479cf69474a21
mjrdnk <mjrdnk@gmail.com>
2016-04-15 23:10:04 +0200

    angular minified added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
 $(document).ready(function() {
 	
-	//preset screens
-	
-	
-	// behaviour on click
-	$('#performer').click(function() {
-		$(this).toggle('slide', {direction: 'right'}, 1000);
-	});
-
-	// behaviour on click
-	$('#lurker').click(function() {
-		$(this).toggle('slide', {direction: 'left'}, 1000);
-	});
-
 });
