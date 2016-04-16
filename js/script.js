acbe3fb9a9c94c398a5e70e56af110fce1657458
mjrdnk <mjrdnk@gmail.com>
2016-04-16 03:54:14 +0200

    minor improvements door functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
-	// $('#performer').css({visibility: 'hidden'});
-	// $('#lurker').css({visibility: 'hidden'});
+
+	// preset main page
+	$('#lurkerCover').css({visibility: 'visible'});
+	$('#performerCover').css({visibility: 'visible'});
+	$('#performer').css({visibility: 'hidden'});
+	$('#lurker').css({visibility: 'hidden'});
+
+	// on click events
+	$('#lurkerCover').click(function() {
+		$(this).hide();
+		$('#lurker').css({visibility: 'visible'});
+	});
+
+	$('#performerCover').click(function() {
+		$(this).hide();
+		$('#performer').css({visibility: 'visible'});
+	});
 });
