0d0cb10aebce5bfef8e7b0ca507948c3969de4b6
mjrdnk <mjrdnk@gmail.com>
2016-04-16 04:18:01 +0200

    minor improvements design

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
 $(document).ready(function() {
 
-	// preset main page
-	$('#lurkerCover').css({visibility: 'visible'});
-	$('#performerCover').css({visibility: 'visible'});
-	$('#performer').css({visibility: 'hidden'});
-	$('#lurker').css({visibility: 'hidden'});
-
-	// on click events
-	$('#lurkerCover').click(function() {
-		$(this).hide();
-		$('#lurker').css({visibility: 'visible'});
-	});
-
-	$('#performerCover').click(function() {
-		$(this).hide();
-		$('#performer').css({visibility: 'visible'});
-	});
 });
