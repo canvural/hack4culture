b0d7eb8b5405fe608730e81def8168a7f87cd0c0
canvural <can9119@gmail.com>
2016-04-16 06:27:24 +0200

    --wip--

--- performer.php
+++ performer.php
@@ -37,7 +37,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-            <h1><?php echo $el['bio'] ?></h1>
+                 <h1><?php echo $el['bio'] ?></h1>
 			</div>
 		</div>
 	</div>
